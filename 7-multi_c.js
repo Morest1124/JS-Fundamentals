f26776dcdcc09d48ae8bacc49097a867c2f7da80
Morest1124 <morest1124burawa2005@gmail.com>
2025-07-11 21:20:05 +0200

    Add if less that or = to zero

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,8 @@
 const args = process.argv[2];
 const times = parseInt(args[0]);
 
-if (isNaN(times)) {
-  console.log();
+if (isNaN(times) <= 0) {
+  console.log("");
 } else {
   for (let x = 0; x < times; x++) {
     console.log("C is fun");
