35738fbae81b8df1b80a7bfecbab62a01bdf2a4b
Morest1124 <morest1124burawa2005@gmail.com>
2025-07-11 16:12:24 +0200

    Add not a number

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,4 +1,10 @@
 const process = require('process')
 
-const myNumber= parseInt(process.argv[2]);
-console.log('My number:', (myNumber))
+const myNumber = parseInt(process.argv[2]);
+
+
+if (myNumber === parseInt(Number(myNumber))) {
+    console.log('My number:', (myNumber))
+} else if (myNumber !== parseInt(Number())) {
+    console.log('Not number')
+}
