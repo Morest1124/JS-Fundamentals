f35d18135237452738c449cb1e9874d4aabc7a5c
Morest1124 <morest1124burawa2005@gmail.com>
2025-07-11 21:34:05 +0200

    add a logic oparator on line 7

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,10 @@
 const args = process.argv[2];
 const times = parseInt(args[0]);
 
-if (isNaN(times) ||  0) {
+if (isNaN(times) || 0) {
   console.log("");
 } else {
-  for (let x = 0; x < times; x++) {
+  for (let x = 0; x < times || x < 0; x++) {
     console.log("C is fun");
   }
 }
