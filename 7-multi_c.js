c21b8cc541040874452d7c3e32a4115ccd5a5e96
Morest1124 <morest1124burawa2005@gmail.com>
2025-07-11 21:07:19 +0200

    Removing Missing number of occurrences

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,8 @@
-
-
 const args = process.argv[2];
 const times = parseInt(args[0]);
 
 if (isNaN(times)) {
-  console.log("Missing number of occurrences");
+  console.log("");
 } else {
   for (let x = 0; x < times; x++) {
     console.log("C is fun");
