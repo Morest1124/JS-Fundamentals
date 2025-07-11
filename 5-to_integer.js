b6d797340d5417719b635c0a46b1b69937dc7ae1
Morest1124 <morest1124burawa2005@gmail.com>
2025-07-11 16:15:38 +0200

    Correct the grammar

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -6,5 +6,5 @@ const myNumber = parseInt(process.argv[2]);
 if (myNumber === parseInt(Number(myNumber))) {
     console.log('My number:', (myNumber))
 } else if (myNumber !== parseInt(Number())) {
-    console.log('Not number')
+    console.log('Not a number')
 }
