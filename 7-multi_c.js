5e08bcfdac11b007e1deb64e932def254168312c
Morest1124 <morest1124burawa2005@gmail.com>
2025-07-11 21:55:25 +0200

    Bugs keep stressing me

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,14 @@
-const args = process.argv[2];
-const times = parseInt(args[0]);
+const args = process.argv.slice(2);
 
-if(args.length === 0 || isNaN(times) || parseInt(args[0])<=0) {
-  console.log("");
-} else {
-  for (let x = 0; x < times; x++) {
-    console.log("C is fun");
+if (args.length === 0 || isNaN(parseInt(args[0])) || parseInt(args[0]) <= 0) {
+  if (args.length === 0 || isNaN(parseInt(args[0]))) {
+    console.log("Missing number of occurrences");
   }
+} else {
+  const times = parseInt(args[0]);
+  let output = "";
+  for (let i = 0; i < times; i++) {
+    output += "C is fun\n";
+  }
+  console.log(output.trim());
 }
