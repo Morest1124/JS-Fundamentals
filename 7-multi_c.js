0b86daf416951d749937f6cd703241b4c41fdc4b
Morest1124 <morest1124burawa2005@gmail.com>
2025-07-11 21:27:50 +0200

    add logic oparator ||

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,7 +1,7 @@
 const args = process.argv[2];
 const times = parseInt(args[0]);
 
-if (isNaN(times) <= 0) {
+if (isNaN(times) ||  0) {
   console.log("");
 } else {
   for (let x = 0; x < times; x++) {
